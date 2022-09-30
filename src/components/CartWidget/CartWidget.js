416a5861b5db65e367bcfff42dcb68cc3f71d635
Jeremy benezuela <benezuelajeremy@gmail.com>
2022-09-29 23:56:55 -0300

    pre entrega 1

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cart from "./assets/logo.webp"
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="cart" />
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <>
+        <nav>
+            <h2>JS<span>Store</span></h2>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
